56a0480bf0b4df888c11579c56c8c36ba26a433e
nezhibitskiy <cross01rus@icloud.com>
2021-11-07 13:12:35 +0300

    Added script for stress tests and added job to travis

---
+++ stress_tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if ! [ -d "stress_tests" ]
+then
+mkdir stress_tests
+fi
+cd stress_tests
+cmake ..
+make
+if ! [ -d "tests/stress_tests/texts" ]
+then
+mkdir tests/stress_tests/texts
+fi
+cd tests/utils/
+echo "Started file generation"
+./file_gen
+echo "Ended file generation"
+cd ../stress_tests/async
+async_output=$(./stress_async_test)
+echo "Async output is: $async_output"
+cd ../sync
+sync_output=$(./stress_sync_test)
+echo "Sync output is: $sync_output"
+if (( $(echo "$async_output > $sync_output" |bc -l) ));
+then
+  echo "Async is slower, than sync"
+else
+  echo "Sync is slower, than async"
+fi
