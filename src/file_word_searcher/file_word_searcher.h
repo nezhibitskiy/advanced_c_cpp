e599d151d58287e5e6b92f0d0dafa2cf69d67912
nezhibitskiy <cross01rus@icloud.com>
2021-11-07 23:07:18 +0300

    Added cpplint fixes

--- src/file_word_searcher/file_word_searcher.h
+++ src/file_word_searcher/file_word_searcher.h
@@ -8,6 +8,6 @@
 #include "file_reader.h"
 #include "searcher.h"
 
-char* file_long_word_search(const char* filename, size_t* word_length);
+char *file_long_word_search(const char *filename, size_t *word_length);
 
 #endif  // SRC_FILE_WORD_SEARCHER_FILE_WORD_SEARCHER_H_
